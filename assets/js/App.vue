63685e731fbdf79afee70085c36b7c6806128073
dubost-clement <braunschweig-dubost@outlook.fr>
2018-05-18 12:10:43 +0200

    first commit

---
+++ assets/js/App.vue
@@ -0,0 +1,15 @@
+<style>
+  @import '../scss/app.scss';
+</style>
+
+<template>
+  <h1>Bonjour {{name}}!</h1>
+</template>
+
+<script>
+  export default {
+    data(){
+      return { name: 'Dubost Clément' }
+    }
+  }
+</script>
---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+// src/Controller/DefaultController.php
+ 
+namespace App\Controller;
+ 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+ 
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function default()
+    {
+        return $this->render('base.html.twig');
+    }
+}
